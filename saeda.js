501b20be442346f546095172ffdfa739d02222ee
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 03:29:27 +0300

    Create saeda.js

---
+++ saeda.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true})
+
+////Saeda
+client.on('message', message => { 
+
+if (!message.guild) return; 
+
+if (message.content === '1wara') { 
+ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+if (message.member.voiceChannel) { 
+
+message.member.voiceChannel.join() 
+
+.then(connection => { 
+
+
+}) 
+
+.catch(console.log); 
+
+} else { 
+
+message.reply('**بچۆ ڤۆیس ئینجا فەرمانکە**'); 
+
+} 
+
+} 
+
+});
+
+
+client.on("message" , message => {
+
+if(message.channel.type === 'dm') {
+
+if (message.content.startsWith("https://discord.gg/")) {
+    
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/3FfXdpg6qG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+    
+  client.channels.get("755636700482633791").send(
+`> By: <@${message.author.id}> ✅ 
+${message.content}`)
+}
+    }
+        })
+
+
+
+
+client.on('ready', () => {
+console.log(`ON ${client.guilds.size} Servers`);
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑️`) 
+});
+
+Saeda up
+
+
+client.login(""); ///تۆکین دانێ 
